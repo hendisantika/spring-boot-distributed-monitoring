be81f091c6bc183ba8cd2dbe2c2ac97abb94c3f4
hendisantika <hendisantika@yahoo.co.id>
2025-03-14 06:29:31 +0700

    feat: Add Core Annotation🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/distributedmonitoring/annotation/Core.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.distributedmonitoring.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-distributed-monitoring
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 14/03/25
+ * Time: 06.29
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@Documented
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Core {
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+}
