437db9bdf9319c1b2192085eb24e2d26cf865f20
hendisantika <hendisantika@yahoo.co.id>
2025-03-14 06:17:54 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/distributedmonitoring/SpringBootDistributedMonitoringApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.distributedmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDistributedMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDistributedMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/distributedmonitoring/SpringBootDistributedMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.distributedmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDistributedMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
