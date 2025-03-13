19f1c96a0bb153f43831e55b44079b48e7ca9983
hendisantika <hendisantika@yahoo.co.id>
2025-03-14 06:37:30 +0700

    feat: Add deleteUser in UserController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/distributedmonitoring/controller/UserController.java
+++ src/main/java/id/my/hendisantika/distributedmonitoring/controller/UserController.java
@@ -7,6 +7,7 @@ import io.micrometer.observation.annotation.Observed;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -54,4 +56,9 @@ public class UserController {
         return ResponseEntity.ok(users);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
